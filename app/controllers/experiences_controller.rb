b993274af149df8894f8a04db0d206af7316222c
meirosilio <meirosilio@gmail.com>
2017-11-12 00:35:21 +0200

    create dashboard layout + basic exprinces layout

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,6 +1,9 @@
 class ExperiencesController < ApplicationController
 
 
+  def index
+    @experiences = Experience.all
+  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'dashboard/index'
+
   get 'experiences/index'
 
   root 'static_pages#index'
